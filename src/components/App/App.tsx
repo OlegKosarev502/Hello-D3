0c5d1266150c8d83425e66a681329e89924562f9
Oleg Kosarev <OlegKosarev502@outlook.com>
2019-11-22 02:02:08 +0300

    Configured dev env

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      Hello D3!
+    </div>
+  );
+}
+
+export default App;
