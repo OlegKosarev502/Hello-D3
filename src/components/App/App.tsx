417fd9ca16b7e238965676381afc09faf7d881f4
Oleg Kosarev <OlegKosarev502@outlook.com>
2019-11-29 01:09:48 +0300

    Tried useState hook.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 const App: React.FC = () => {
+  const [count, setCount] = useState(3);
+
   return (
-    <div className="App">
-      Hello D3!
+    <div 
+      className="App"
+      onClick={() => setCount(count + 1)}
+      onMouseDown={e => e.preventDefault()}
+    >
+      {`Hello D${count}!`}
     </div>
   );
 }
